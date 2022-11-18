da87b438a07aef18582a04c4d657fe9fd4470f8a
cabralcaio <caio_cabral1@hotmail.com>
2022-11-18 12:32:00 -0300

    Alteração IBGE time

---
+++ app-colaboradores/src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyBsim1zwUosA1qkox0C9PEILDflOED4FJY",
+        authDomain: "app-colaboradores-e3d3c.firebaseapp.com",
+        projectId: "app-colaboradores-e3d3c",
+        storageBucket: "app-colaboradores-e3d3c.appspot.com",
+        messagingSenderId: "956812975970",
+        appId: "1:956812975970:web:86ddf9a7b57603df83563b",
+        measurementId: "G-NVER0RN0K4"
+      }
+}
